99a4e3cd211d6148726f62b5879d39b9afd97977
Zurc PC <ayyogabe@gmail.com>
2016-10-03 15:03:06 -0700

    Color Drop v0.02

---
+++ Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMovement : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float speed;
+
+
+	//Changes the speed of the floor
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.up * speed;
+	}
+}
+
+// create code that increases the speed of the floor over time
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+
+	public GameObject floor;
+	public Vector3 spawnValues;
+	public int floorCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start() 
+	{
+		StartCoroutine (FloorWaves ());
+	}
+
+	IEnumerator FloorWaves ()
+	{
+		Debug.Log ("Floor Waves Start");
+		//start wait so that player has time to get ready, no decreasing value here
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			Debug.Log ("While Statement Activates");
+			for (int i = 0; i < floorCount; i++)
+			{
+				Debug.Log ("Floor Generator Starts?");
+			//enter random color code here
+			//GameObject hazard = hazards Random
+			Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, spawnValues.z); //spawn position for floors
+			Quaternion spawnRotation = Quaternion.identity; //needed quaternion values for the instatiate? Not sure if used
+			Instantiate (floor, spawnPosition, spawnRotation); //I believe this spawns the floors
+			yield return new WaitForSeconds (spawnWait); //needs to add a decreasing spawnWait timer
+			}
+		yield return new WaitForSeconds (waveWait); //needs to add a decreasing waveWait timer
+		}
+	}
+}
